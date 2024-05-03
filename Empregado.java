6332a36409e048da60c50dec573910300455d2a3
HelenaPicinin <137627315+HelenaPicinin@users.noreply.github.com>
2024-05-03 11:44:08 -0300

    Update Empregado.java

--- Empregado.java
+++ Empregado.java
@@ -1,13 +1,50 @@
-import java.util.Scanner;
-
 public class Empregado {
-    public static void main(String[] args){
-        
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite seu CPF: ");
-        String cpf = scanner.nextLine();
+        private String nome;
 
-        
-    }
+        private Long cpf;
+        private int datanasc;  
+        private int senha;            
+
+    
+        // Construtor
+        public Empregado(String nome, Long cpf, int datanasc, int senha) {
+            this.nome = nome;
+            this.cpf = cpf;
+            this.datanasc = datanasc;
+            this.senha = senha;
+        }
+    
+        // Métodos getters e setters
+        public String getNome() {
+            return nome;
+        }
+    
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+    
+        public Long getCpf() {
+            return cpf;
+        }
+    
+        public void setCpf(Long cpf) {
+            this.cpf = cpf;
+        }
+    
+        public int getDatanasc() {
+            return datanasc;
+        }
+    
+        public void setDatanasc(int datanasc) {
+            this.datanasc = datanasc;
+        }
+    
+        public void setSenha(int senha) {
+            this.senha = senha;
+        }
+
+        public int getSenha() {
+            return senha;
+        }
 }
