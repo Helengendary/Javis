60aa00a9858610384fb1f3e160130683eaf35ecd
HelenaPicinin <137627315+HelenaPicinin@users.noreply.github.com>
2024-05-03 11:45:47 -0300

    Update NoEmpregados.java

--- NoEmpregados.java
+++ NoEmpregados.java
@@ -1,20 +1,22 @@
-public class NoEmpregados {
+class NoEmpregados
+{
+    // Atributo next
     private NoEmpregados next = null;
     NoEmpregados getNext() {
         return next;
     }
-
     void setNext(NoEmpregados next) {
         this.next = next;
     }
-    
+
+    // Atributo Empregado
     private Empregado value;
     Empregado getEmpregado() {
         return value;
     }
 
+    // Construtor
     NoEmpregados(Empregado value) {
         this.value = value;
     }
-    
-}
+}   
