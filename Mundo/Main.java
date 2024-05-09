e03ef88a5a2f7fc721f0c4428e88327f06df417d
HelenaPicinin <137627315+HelenaPicinin@users.noreply.github.com>
2024-05-09 11:53:29 -0300

    Main

---
+++ Mundo/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+
+    public static void main(String[] args) {
+    
+        Mundo mundo = new Mundo();
+
+        mundo.CriarMundo(1, 123);
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+        mundo.Rodada();
+
+       
+    }
+}
