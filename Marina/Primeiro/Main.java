25dfacdc51beae8c5415b7be34eff0e5c378d0ba
Helengendary <helenapicinin13@gmail.com>
2024-08-08 22:05:43 -0300

    Tem que fazer o exercício

--- Marina/Primeiro/Main.java
+++ Marina/Primeiro/Main.java
@@ -1,6 +1,28 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        Escrita lapis = new Escrita("BIC", "Grafite");
+        Escrita caneta = new Escrita("BIC", "Tinta");
+        Escrita marcaTexto = new Escrita("BIC", "Tinta florecente");
 
-        System.out.println("Hello world!");
+        System.out.println("\nLápis\n");
+
+        lapis.setCor("Rosa");
+        lapis.setEspessura(0.7F);
+        lapis.setTamanho(12F);
+        lapis.escrever("Comprar Biscoito");
+        lapis.escrever("Comprar pulseira");
+
+        System.out.println("\nCaneta\n");
+
+        caneta.setCor("Azul");
+        caneta.setTamanho(40F); 
+        caneta.setEspessura(0.9F);
+        caneta.escrever("Helena");
+        caneta.escrever("Picinin");
+
+        System.out.println("\nMarca Texto\n");
+
+        marcaTexto.setCor("Amarelo");
+        marcaTexto.setTamanho(20F);
     }
 }
--- Marina/Primeiro/utensilio.java
+++
@@ -1,6 +0,0 @@
-public class utensilio {
-    public String material;
-    public String cor;
-
-    public void escrever(){};
-}
